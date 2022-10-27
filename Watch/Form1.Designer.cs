7659f68d0433a37c3f4a93353413b4ef7897d423
Andrey Popov <andrej_1987@bk.ru>
2022-10-27 21:31:20 +0500

    Добавьте файлы проекта.

---
+++ Watch/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Watch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelWatch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelWatch
+            // 
+            this.labelWatch.AutoSize = true;
+            this.labelWatch.Font = new System.Drawing.Font("MV Boli", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWatch.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.labelWatch.Location = new System.Drawing.Point(83, 129);
+            this.labelWatch.Name = "labelWatch";
+            this.labelWatch.Size = new System.Drawing.Size(359, 157);
+            this.labelWatch.TabIndex = 0;
+            this.labelWatch.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelWatch);
+            this.Name = "Form1";
+            this.Text = "Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelWatch;
+    }
+}
+
---
+++ Watch/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Watch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += new EventHandler(timer1_Tick);
+        }
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            labelWatch.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+       
+    }
+}
